791ad04108bd045dee62eb8ce6fbe7056a81ada6
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-21 18:23:31 -0800

    Add message service

---
+++ services/messageService.ts
@@ -0,0 +1,19 @@
+const API_ENDPOINT = "http://localhost:8000/api/messages";
+
+export const fetchMessages = async () => {
+  const response = await fetch(API_ENDPOINT);
+  const data = await response.json();
+  return data;
+};
+
+export const createMessage = async (message: string[]) => {
+  const response = await fetch(API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+  const data = await response.json();
+  return data;
+};
