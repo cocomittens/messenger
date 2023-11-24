3d142efd5b5f5ee31fc47f0be980699f17fdd9a1
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-24 14:57:54 -0800

    Add text field component

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { fetchMessages } from "@/services/messageService";
 import { MessageType } from "@/types/Chat";
 import Message from "@/components/Message";
+import { TextField } from "@material-ui/core";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -12,13 +13,12 @@ const Chat = () => {
     fetchMessages().then((messages) => setMessages(messages));
   }, []);
 
-  console.log(messages);
-
   return (
     <div>
       {messages.map((message: MessageType) => (
         <Message key={message.id} {...message} />
       ))}
+      <TextField />
     </div>
   );
 };
