e03a2f6f1892622911c1ce902d740c1404fbcad2
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-24 18:59:45 -0800

    Add header

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,7 +4,14 @@ import React, { useEffect, useState } from "react";
 import { fetchMessages, createMessage } from "@/services/messageService";
 import { MessageType } from "@/types/Chat";
 import Message from "@/components/Message";
-import { TextField, Box, IconButton, Grid } from "@mui/material";
+import {
+  TextField,
+  Box,
+  IconButton,
+  Grid,
+  Typography,
+  Avatar,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 const Chat = () => {
@@ -35,6 +42,14 @@ const Chat = () => {
       alignItems="center"
       justifyContent="center"
     >
+      <Grid container item width="60vw" alignItems="center" p={1}>
+        <Grid item>
+          <Avatar src="https://pusheen.com/wp-content/uploads/2020/12/What-Sweet-Quiz-SocialResults_Donut-1-e1608220861325.jpg" />
+        </Grid>
+        <Grid item px={1}>
+          <Typography color="#111">ilovecats420</Typography>
+        </Grid>
+      </Grid>
       <Grid
         container
         direction="column"
