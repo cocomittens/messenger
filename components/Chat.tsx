29f3d323d87e3de16cd29fe08d49346efcb7b02c
Corrie Stoddard <corriestoddard@gmail.com>
2023-11-26 18:56:46 -0800

    Positioning

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -3,11 +3,12 @@
 import React, { useEffect, useState } from "react";
 import { fetchMessages, createMessage } from "@/services/messageService";
 import { MessageType } from "@/types/Chat";
-import Message from "@/components/Message";
 import { TextField, Box, IconButton, Grid } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import Header from "./Header";
 import { fetchUserProfile } from "@/services/messageService";
+import Header from "./Header";
+import Message from "@/components/Message";
+import Contacts from "@/components/Contacts";
 
 const Chat = () => {
   const [message, setMessage] = useState("");
@@ -45,45 +46,53 @@ const Chat = () => {
       alignItems="center"
       justifyContent="center"
     >
-      <Header username={user.username} image={user.image} />
-      <Grid
-        container
-        direction="column"
-        width="60vw"
-        height="80vh"
-        justifyContent="flex-end"
-        bgcolor="rgba(255, 255, 255, 0.5)"
-        borderRadius={2}
-        p={2}
-        onKeyDown={(e) => {
-          e.key === "Enter" && handleSend();
-        }}
-      >
-        {messages.map((message: MessageType) => (
-          <Message key={message.id} {...message} {...user} />
-        ))}
-        <Grid
-          container
-          item
-          direction="row"
-          justifyContent="space-between"
-          mt={2}
-        >
-          <Grid item xs={11}>
-            <TextField
-              color="secondary"
-              multiline
-              placeholder="Enter message..."
-              variant="outlined"
-              fullWidth
-              onChange={(e) => setMessage(e.target.value)}
-              value={message}
-            />
-          </Grid>
-          <Grid container item xs={1} justifyContent="center">
-            <IconButton color="secondary" onClick={handleSend}>
-              <SendIcon />
-            </IconButton>
+      <Grid container>
+        <Grid container item xs={3}>
+          <Contacts contacts={[user]} />
+        </Grid>
+        <Grid container item xs={9}>
+          <Header username={user.username} image={user.image} />
+
+          <Grid
+            container
+            direction="column"
+            width="60vw"
+            height="80vh"
+            justifyContent="flex-end"
+            bgcolor="rgba(255, 255, 255, 0.5)"
+            borderRadius={2}
+            p={2}
+            onKeyDown={(e) => {
+              e.key === "Enter" && handleSend();
+            }}
+          >
+            {messages.map((message: MessageType) => (
+              <Message key={message.id} {...message} {...user} />
+            ))}
+            <Grid
+              container
+              item
+              direction="row"
+              justifyContent="space-between"
+              mt={2}
+            >
+              <Grid item xs={11}>
+                <TextField
+                  color="secondary"
+                  multiline
+                  placeholder="Enter message..."
+                  variant="outlined"
+                  fullWidth
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                />
+              </Grid>
+              <Grid container item xs={1} justifyContent="center">
+                <IconButton color="secondary" onClick={handleSend}>
+                  <SendIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { Grid, Typography, Avatar } from "@mui/material";
+import Contact from "./Contact";
 
 type User = {
   username: string;
@@ -7,16 +6,7 @@ type User = {
 };
 
 const Header = ({ username, image }: User) => {
-  return (
-    <Grid container item width="60vw" alignItems="center" p={1}>
-      <Grid item>
-        <Avatar src={image} />
-      </Grid>
-      <Grid item px={1}>
-        <Typography color="#111">{username}</Typography>
-      </Grid>
-    </Grid>
-  );
+  return <Contact username={username} image={image} />;
 };
 
 export default Header;
